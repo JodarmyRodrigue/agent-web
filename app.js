69a6f32c0b7ad9c9b51a9b56b2711191492fed05
bsrodrigue <bsrodrigue@gmail.com>
2020-09-04 15:57:33 +0000

    Create Agents route and page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/auth/login');
 var registerRouter = require('./routes/auth/register');
 var accountRouter = require('./routes/dashboard/account');
+var agentsRouter = require('./routes/dashboard/agents');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/auth/login', loginRouter);
 app.use('/auth/register', registerRouter);
 
 app.use('/dashboard/account', accountRouter);
+app.use('/dashboard/agents', agentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
