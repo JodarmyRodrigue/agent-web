cd76f1b817c464a713c012071f3794309770135e
bsrodrigue <bsrodrigue@gmail.com>
2020-09-04 18:11:24 +0000

    Setup Redis Client and Session

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var redis = require('redis');
+var RedisStore = require('connect-redis')(session);
 
+
+
+//Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -25,12 +31,35 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+//Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var redisClient;
+var sessionSecret;
+
+if(process.env.ENV === 'DEV'){
+  console.log('Environment: Developpement');
+  redisClient = redis.createClient();
+  sessionSecret = 'aStOlfO__Is_bEsT_Giirrrl';
+}else{
+  console.log('Environnment Production');
+  redisClient = redis.createClient(process.env.REDIS_URL);
+  sessionSecret = process.env.SESSION_SECRET;
+}
+
+app.use(session({
+  store: new RedisStore({
+    client: redisClient
+  }),
+  secret: sessionSecret,
+  resave: true,
+  saveUninitialized: true,
+}));
 
 // Routers
 app.use('/', indexRouter);
