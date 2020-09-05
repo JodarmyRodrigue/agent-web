2cb9b7e5a6a8411a1f6ebe61472817282dd33b74
bsrodrigue <bsrodrigue@gmail.com>
2020-09-05 02:14:36 +0000

    Setup Login Request with http error handling

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,9 +1,76 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+
+let baseUrl;
+if (process.env.ENV === 'DEV') {
+  baseUrl = "http://127.0.0.1:1337";
+} else {
+  baseUrl = "https://agent-api";
+}
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
-  res.render('auth/login', { title: 'Connexion' });
+router.get('/', function (req, res, next) {
+  res.render('auth/login', {
+    title: 'Connexion'
+  });
+});
+
+router.post('/', function (req, res, next) {
+  if (!req.body) {
+    res.status(400).send({
+      message: 'Veuillez fournir des données'
+    });
+    res.end();
+  }
+
+  axios.post(`${baseUrl}/auth/local`, req.body, {
+    timeout: 30 * 1000,
+  }).then(response => {
+    console.log(response);
+  }).catch(error => {
+    if (error.response) {
+      if (error.response.data.statusCode === 400) {
+        let errorMessage = 'Erreur';
+        try {
+          errorMessage = error.response.data.message[0].messages[0].message;
+        } catch (error) {
+          console.log('Error while getting error message :', error);
+        }
+        console.log('Bad Request :', errorMessage);
+        res.status(400).send({
+          message: errorMessage,
+        });
+
+      } else if (error.response.data.statusCode === 403) {
+
+        console.log('Forbidden Request');
+        res.status(403).send({
+          message: 'Interdit'
+        });
+
+      } else if (error.response.data.statusCode === 404) {
+
+        console.log('Not Found');
+        res.status(404).send({
+          message: 'Not Found'
+        });
+
+      }
+    } else if (error.request) {
+      console.log(error.request);
+      res.status(500).send({
+        message: 'Pas de réponse'
+      });
+    } else {
+      console.log(error);
+      res.status(500).send({
+        message: 'Erreur Inconnue'
+      });
+    }
+  }).finally(() => {
+
+  });
 });
 
 module.exports = router;
