e96f520f11b31225bf2f098fad76911273b4e21b
bsrodrigue <bsrodrigue@gmail.com>
2020-09-06 15:42:54 +0000

    Improve isAuthenticated Middleware

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -3,7 +3,18 @@ module.exports = function isAuthenticated(req, res, next) {
         console.log('User is Authenticated');
     } else {
         console.log('User is not Authenticated');
-        res.redirect('/auth/login');
+        if (req.method === 'GET') {
+            res.redirect('/auth/login');
+        } else if (req.method === 'POST') {
+            res.status(400).json({
+                message: "VOUS N'AVEZ PAS LA PERMISSION"
+            });
+        } else {
+            res.status(400).json({
+                message: "VOUS N'AVEZ PAS LA PERMISSION"
+            });
+        }
+
     }
     next();
 };
