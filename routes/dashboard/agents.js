76c66419a870eabc2fb8b447b0dc198eda84f542
bsrodrigue <bsrodrigue@gmail.com>
2020-09-06 10:30:06 +0000

    Add Agent Listing

--- routes/dashboard/agents.js
+++ routes/dashboard/agents.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-/* GET account page. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard/agents', { title: 'Liste des agents', user: req.session.user, jwt: req.session.jwt });
+let baseUrl;
+if (process.env.ENV === 'DEV') {
+  baseUrl = "http://127.0.0.1:1337";
+} else {
+  baseUrl = "https://agent-api";
+}
+
+router.get('/', function (req, res, next) {
+  let agents = [];
+  axios.get(`${baseUrl}/users?web_user=false`, {
+    timeout: 30 * 1000,
+    headers: {'Authorization':`Bearer ${req.session.jwt}`},
+  }).then(response => {
+    console.log("Agents retrieved successfully!");
+    agents = response.data;
+  }).catch(error => {
+    console.log('An error occured while getting agents');
+    if (error.response) {
+      console.log('Error :', error.response.data);
+    } else {
+      console.log('Error :', error.request);
+    }
+  }).finally(() => {
+    console.log(agents);
+    res.render('dashboard/agents', {
+      title: 'Liste des agents',
+      user: req.session.user,
+      jwt: req.session.jwt,
+      agents: agents
+    });
+  });
 });
 
 module.exports = router;
