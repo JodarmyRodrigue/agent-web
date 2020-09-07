c673a4904b4a4932dfc954ecd99640a49a2b0884
bsrodrigue <bsrodrigue@gmail.com>
2020-09-07 02:53:11 +0000

    Comment out shit

--- public/javascripts/dashboard/forms/contact.js
+++ public/javascripts/dashboard/forms/contact.js
@@ -1,8 +1,26 @@
-let $buttons = document.querySelectorAll('button');
+// let $buttons = document.querySelectorAll('button');
 
-$buttons.forEach(button => {
-    button.addEventListener('click', event => {
-        event.preventDefault();
-        console.log("Access Sheet #", button.id);
-    });
-});
+// $buttons.forEach(button => {
+//     button.addEventListener('click', event => {
+//         event.preventDefault();
+//         console.log("Access Sheet #", button.id);
+
+//         axios.get('/dashboard/forms/contact/download', {
+//             sheetId: button.id,
+//         }, {
+//             headers: {"Content-type": "application/pdf"}
+//         }).then(response => {
+//             const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
+//             const link = document.createElement('a');
+//             link.href = downloadUrl;
+//             link.setAttribute('download', 'fichier.pdf');
+//             document.body.appendChild(link);
+//             link.click();
+//             link.remove();
+
+//             console.log(response.data);
+//         }).catch(error => {
+//             console.log(error);
+//         })
+//     });
+// });
