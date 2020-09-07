b1fc0cf403abe72e6902f00f4ded64460637f613
bsrodrigue <bsrodrigue@gmail.com>
2020-09-07 02:53:00 +0000

    Can download sheets as pdf files

--- routes/dashboard/forms/contact.js
+++ routes/dashboard/forms/contact.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var isAuthenticated = require('../../../middlewares/isAuthenticated');
 var axios = require('axios');
+var PDFDocument = require('pdfkit');
+var fs = require('fs');
 
 let baseUrl;
 if (process.env.ENV === 'DEV') {
@@ -10,11 +12,59 @@ if (process.env.ENV === 'DEV') {
   baseUrl = "https://agent-api";
 }
 
-router.get('/', isAuthenticated,  function(req, res, next) {
+router.get('/download', isAuthenticated, async function (req, res, next) {
+  if (!req.query.id) {
+    console.log("No query id...");
+  } else {
+    axios.get(`${baseUrl}/contact-registration-sheets?id=${req.query.id}`, {
+      timeout: 30 * 1000,
+      headers: {
+        'Authorization': `Bearer ${req.session.jwt}`
+      },
+    }).then(response => {
+      const doc = new PDFDocument();
+      const {
+        id,
+        instigator_name,
+        instigator_number,
+        case_id,
+        firstname,
+        lastname,
+        household_chief_name,
+        case_location,
+        address,
+        community_chief_name,
+        symptoms_date,
+        sanitary_district,
+        region,
+        contact_infos
+      } = response.data[0];
+
+
+      res.setHeader('Content-disposition', 'attachment; filename="file.pdf"');
+      res.setHeader('Content-type', 'application/pdf');
+
+      doc.fontSize(18)
+        .text(`Nom de l'Instigateur : ${instigator_name}`)
+        .text(`Numéro de l'Instigateur : ${instigator_number}`)
+        .text(`Nom : ${firstname}`)
+        .text(`Prénom : ${lastname}`)
+        .text(`Lieu d'identification du cas : ${case_location}`);
+      doc.pipe(res);
+      doc.end();
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+});
+
+router.get('/', isAuthenticated, function (req, res, next) {
   let sheets = [];
   axios.get(`${baseUrl}/contact-registration-sheets?_sort=created_at:DESC`, {
     timeout: 30 * 1000,
-    headers: {'Authorization':`Bearer ${req.session.jwt}`},
+    headers: {
+      'Authorization': `Bearer ${req.session.jwt}`
+    },
   }).then(response => {
     console.log("Sheets retrieved successfully!");
     sheets = response.data;
